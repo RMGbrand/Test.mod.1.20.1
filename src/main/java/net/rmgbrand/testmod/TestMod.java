7e2c8eb3796724eb366798205fec85fb24718eed
RMGbrand <omegaflame2007@gmail.com>
2024-09-23 17:52:11 -0400

    Just did some changes

--- src/main/java/net/rmgbrand/testmod/TestMod.java
+++ src/main/java/net/rmgbrand/testmod/TestMod.java
@@ -16,9 +16,9 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TestMod.MOD_ID)
 public class TestMod {
-    // Define mod id in a common place for everything to reference
+
     public static final String MOD_ID = "testmod";
-    // Directly reference a slf4j logger
+
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public TestMod() {
